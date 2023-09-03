0057ff124270fb2ce7c5f1ba59660e90e59dc193
Techi-Joe <techi.jo3@gmail.com>
2023-09-03 00:01:44 -0400

    got display_text working, and some core functions

--- src/datamuse_api.py
+++ src/datamuse_api.py
@@ -1,11 +1,7 @@
 import requests
 
-def fetch_words_from_api():
-    # API request code
-    return 0
+url = "https://api.datamuse.com/words?topics="
 
-def process_api_response(response_data):
-    # Process API response data
-    return 0
-
-# Other API-related functions
+def fetch_words_from_api(topic):
+    # API request
+    return requests.get(url + topic.lower(), timeout=5)
--- src/hangman.py
+++ src/hangman.py
@@ -1,13 +1,33 @@
+'''
+This is the hangman module for running the
+primary game loop.
+'''
+
+import time
+from colorama import Fore, Back, Style, init
+import datamuse_api
+
 class HangmanGame:
-    def __init__(self, word):
-        self.word = word
+    def __init__(self):
+        self.word = "-1"
         self.guessed_letters = set()
         self.max_attempts = 6
         self.remaining_attempts = self.max_attempts
 
     def play(self):
         # Main game loop and logic
-        print("Starting game...")
+        intro_texts = ["Welcome to Hangman Game, designed by Techi-Joe!", "ahhhhhhhhhhh"]
+        for i in range(len(intro_texts)):
+            self.display_text(intro_texts[i])
+
+    def display_text(self, text):
+        # Display the text using a enter to continue method
+        print(Fore.GREEN + "\r" + text)
+        print(Fore.LIGHTBLACK_EX + "Press enter to continue...", end="", flush=True)
+        input()
+        print(Style.RESET_ALL + "\033[A", end="")
+        print("\r                          ", end="")
+
     def display_word(self):
         # Display the word with guessed letters revealed
         return 0
